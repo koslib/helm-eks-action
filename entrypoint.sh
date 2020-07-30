2b210c689b2d3abfc217cc86a469926238f29d45
Kostas Livieratos <koslibpro@gmail.com>
2020-07-30 17:31:03 +0300

    Changed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
-#!/bin/sh -l
+#!/bin/sh
+
+set -e
 
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
-exec "$1"
-result="$($1)"
+# result="$($1)"
 
-status=$?
-echo ::set-output name=result::$result
-echo "$result"
-if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
+# status=$?
+# echo ::set-output name=result::$result
+# echo "$result"
+# if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
+
+sh -c " $*"
