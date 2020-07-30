c5d3ac519ff29435ff9e13274021cd2aa0889f84
Kostas Livieratos <koslibpro@gmail.com>
2020-07-30 17:00:07 +0300

    Execute subcommand without wrapping

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@ echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 # export HELM_PLUGINS=/usr/local/.helm/plugins/
 
-result="$($1)"
+$1
+# result="$($1)"
 
-status=$?
-echo ::set-output name=result::$result
-echo "$result"
-if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
+# status=$?
+# echo ::set-output name=result::$result
+# echo "$result"
+# if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
