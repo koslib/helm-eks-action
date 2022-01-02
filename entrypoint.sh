26a8746cddcc464e7123a83f36ce92145563559e
koslib <koslib@protonmail.ch>
2022-01-02 21:26:11 +0200

    Added support for optional helm plugins

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,16 @@ set -e
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG="${PWD}/kubeconfig"
 
+plugins=$(echo $INPUT_PLUGINS | tr ",")
+
+for plugin in $plugins
+do
+    echo "installing helm plugin: [$plugin]"
+    helm plugin install $plugin
+done
+
 echo "running entrypoint command(s)"
 
-response=$(sh -c " $*")
+response=$(sh -c " $INPUT_COMMAND")
 
 echo "::set-output name=response::$response"
