0b26aee480b80ad6c1999b56ea6e1546a22ea64d
koslib <koslib@protonmail.ch>
2022-01-02 21:39:43 +0200

    Install helm plugins only if variable is not empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,16 @@ set -e
 echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG="${PWD}/kubeconfig"
 
-plugins=$(echo $INPUT_PLUGINS | tr ",")
+if [-z $INPUT_PLUGINS]
+then
+    plugins=$(echo $INPUT_PLUGINS | tr ",")
 
-for plugin in $plugins
-do
-    echo "installing helm plugin: [$plugin]"
-    helm plugin install $plugin
-done
+    for plugin in $plugins
+    do
+        echo "installing helm plugin: [$plugin]"
+        helm plugin install $plugin
+    done
+fi
 
 echo "running entrypoint command(s)"
 
