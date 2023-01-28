aa7b3b8082e88dc940d3d11d63b75bc370e61a2e
Miguel Ferreira <miguelferreira@me.com>
2023-01-28 18:03:17 +0100

    Consistent use of double quotes arround env vars
    ... except arround the ${plugins} variabel used in the for loop because
    that would prevent word split and make the loop execute only once,
    instead of once per word (ie. plugin)

--- dockerhub/entrypoint.sh
+++ dockerhub/entrypoint.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
+echo "${KUBE_CONFIG_DATA}" | base64 -d > kubeconfig
 export KUBECONFIG="${PWD}/kubeconfig"
-chmod 600 ${PWD}/kubeconfig
+chmod 600 "${PWD}/kubeconfig"
 
 if [[ -n "${INPUT_PLUGINS// /}" ]]
 then
-    plugins=$(echo ${INPUT_PLUGINS} | tr ",")
+    plugins="$(echo "${INPUT_PLUGINS}" | tr ",")"
 
     for plugin in ${plugins}
     do
         echo "installing helm plugin: [${plugin}]"
-        helm plugin install ${plugin}
+        helm plugin install "${plugin}"
     done
 fi
 
@@ -21,6 +21,6 @@ echo "running entrypoint command(s)"
 
 response=$(sh -c " ${INPUT_COMMAND}")
 
-echo "response<<EOF" >> ${GITHUB_OUTPUT}
-echo "$response" >> ${GITHUB_OUTPUT}
-echo "EOF" >> ${GITHUB_OUTPUT}
+echo "response<<EOF" >> "${GITHUB_OUTPUT}"
+echo "$response" >> "${GITHUB_OUTPUT}"
+echo "EOF" >> "${GITHUB_OUTPUT}"
