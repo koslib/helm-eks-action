8f409fa7d563bda4fe9a2a63f916b72755b71aef
rodrigo.silva <rodrigo.silva@bankly.com.br>
2023-11-30 21:55:54 -0300

    up

--- dockerhub/entrypoint.sh
+++ dockerhub/entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-echo "creating docker image arch: $INPUT_ARCH"
-
 echo "${KUBE_CONFIG_DATA}" | base64 -d > kubeconfig
 export KUBECONFIG="${PWD}/kubeconfig"
 chmod 600 "${PWD}/kubeconfig"
@@ -21,7 +19,25 @@ fi
 
 echo "running entrypoint command(s)"
 
-response=$(bash -c "${INPUT_COMMAND}")
+TEST_INPUT_COMMAND=$(cat <<EOF
+helm repo add helm_bankly https://acesso-bankly.github.io/bankly-helm-chart
+helm repo update
+
+helm upgrade -f deployment/values.yaml -i core-scouter helm_bankly/bankly-product --namespace antifraud-transactional --output json 
+--set containerImage=***.dkr.ecr.us-east-1.amazonaws.com/antifraud-core-scouter:1.11.35-alpha,
+environment=staging,
+region=us-east-1,
+tagResources.context=Transactional,
+tagResources.type=specific,
+tagResources.cc=909,
+tagResources.product=Limit
+
+EOF
+)
+
+echo "running command test: [${TEST_INPUT_COMMAND}]"
+
+response=$(bash -c "${TEST_INPUT_COMMAND}")
 
 {
   echo "response<<EOF";
