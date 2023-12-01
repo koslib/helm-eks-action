9ceae3292298261c3b1cbe6a27deb1e058ce10be
rodrigo.silva <rodrigo.silva@bankly.com.br>
2023-11-30 22:17:03 -0300

    update

--- dockerhub/entrypoint.sh
+++ dockerhub/entrypoint.sh
@@ -17,28 +17,7 @@ then
     done
 fi
 
-echo "running entrypoint command(s)"
-
-TEST_INPUT_COMMAND=$(cat <<EOF
-helm repo add helm_bankly https://acesso-bankly.github.io/bankly-helm-chart
-helm repo update
-
-helm upgrade -f deployment/values.yaml -i core-scouter helm_bankly/bankly-product --namespace antifraud-transactional --output json 
---set containerImage=***.dkr.ecr.us-east-1.amazonaws.com/antifraud-core-scouter:1.11.35-alpha,
-environment=staging,
-region=us-east-1,
-tagResources.context=Transactional,
-tagResources.type=specific,
-tagResources.cc=909,
-tagResources.product=Limit
-
-EOF
-)
-
-echo "running command test: [${TEST_INPUT_COMMAND}]"
-
-response=$(bash -c "${TEST_INPUT_COMMAND}")
-
+response=$(bash -c "$INPUT_COMMAND")
 {
   echo "response<<EOF";
   echo "$response";
